78b6b6f251d9c731b18d1d5dcf406a4442398176
Luis Felipe Ortiz-Clavijo <felipe2515@hotmail.com>
2022-07-05 16:07:28 -0500

    Update mapper.py

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # parse the record into fields
+    fields = line.split('   ')
+    print('%s,%s' % (fields[0], 1))
