1a1dc507817e44f8539a4fce5e09c9862ce259a5
Luis Felipe Ortiz-Clavijo <felipe2515@hotmail.com>
2022-07-05 11:45:35 -0500

    Update reducer.py

--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,3 +1,26 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+import collections
+
+counts = dict()
+
+# input comes from STDIN
+for line in sys.stdin:
+    # parse the input we got from mapper.py
+    credit_history, cnt = line.split(',')
+    
+    # convert year and price (currently a string) to int/float
+    try:
+        cnt = int(cnt)
+    except ValueError as e:
+        continue
+    
+    if credit_history in counts:
+        counts[credit_history] += cnt
+    else:
+        counts[credit_history] = cnt
+
+for k, v in collections.OrderedDict(sorted(counts.items())).items():
+    print('%s\t%d' %(k, v))
