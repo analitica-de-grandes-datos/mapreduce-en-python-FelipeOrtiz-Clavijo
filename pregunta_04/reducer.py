5ed9ec6d5e4e46dd67b9c6dfde083740e3bfde40
Luis Felipe Ortiz-Clavijo <felipe2515@hotmail.com>
2022-07-05 16:07:50 -0500

    Update reducer.py

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -1,3 +1,26 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+import collections
+
+counts = dict()
+
+# input comes from STDIN
+for line in sys.stdin:
+    # parse the input we got from mapper.py
+    col, val = line.split(',')
+    
+    # convert count to int/float
+    try:
+        val = int(val)
+    except ValueError as e:
+        continue
+    
+    if col in counts:
+        counts[col] += val
+    else:
+        counts[col] = val
+
+for k, v in collections.OrderedDict(sorted(counts.items())).items():
+    print('%s,%d' %(k, v))
