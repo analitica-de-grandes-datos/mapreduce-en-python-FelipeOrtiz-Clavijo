77d2adc006b0818757f08fd7bec633c005a5c809
Luis Felipe Ortiz-Clavijo <felipe2515@hotmail.com>
2022-07-05 16:29:57 -0500

    Update mapper.py

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # parse the record into fields
+    fields = line.split('   ')
+    dt = fields[1]
+    month = dt.split('-')[1]
+    print('%s,%s' % (month, 1))
