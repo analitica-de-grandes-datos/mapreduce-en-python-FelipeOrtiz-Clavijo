69f06a30d9049bfa7dd8b3a391f0b9351bab605f
Luis Felipe Ortiz-Clavijo <felipe2515@hotmail.com>
2022-07-05 16:46:35 -0500

    Update reducer.py

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,27 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+import collections
+
+data_list = dict()
+
+# input comes from STDIN
+for line in sys.stdin:
+    # parse the input we got from mapper.py
+    col3, row = line.split(',')
+    
+    # convert count to int/float
+    try:
+        col3 = int(col3)
+    except ValueError as e:
+        continue
+
+    data_list[col3] = row
+
+cnt=0
+for k, v in collections.OrderedDict(sorted(data_list.items())).items():
+    if cnt > 5:
+        break
+    print('%s' %(v), end='')
+    cnt += 1
